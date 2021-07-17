a7fd56c6e405e88c20b6b973422508814f91b576
Daniel Firth <dan.firth@homotopic.tech>
2021-07-17 13:43:54 +0200

    Introduce path-formatting, simple-media-timestamp and simple-media-timestamp-formatting as dependencies

--- src/Polysemy/Video.hs
+++ src/Polysemy/Video.hs
@@ -20,26 +20,12 @@ import Control.Monad.IO.Class
 import Data.Text (Text)
 import qualified Data.Text as T
 import Formatting
+import Media.Timestamp
+import Media.Timestamp.Formatting
 import Path
-import Path.Utils
+import Path.Formatting
 import Polysemy
 import qualified Turtle as S
-
--- | Timestamp data type.
-data Time = Time
-  { hour :: Int,
-    minutes :: Int,
-    seconds :: Int,
-    frame :: Int
-  }
-  deriving (Eq, Ord, Show)
-
--- | Interval of two timestamps.
-data Range = Range
-  { from :: Time,
-    to :: Time
-  }
-  deriving (Eq, Ord, Show)
 
 -- |
 data ClipProcess m a where
@@ -49,25 +35,21 @@ data ClipProcess m a where
 
 makeSem ''ClipProcess
 
--- | Format a Timestamp to ffmpeg's format 00:00:00.000.
-timeFF :: Time -> Text
-timeFF (Time h m s f) = sformat (int % ":" % int % ":" % int % "." % int) h m s f
-
 -- | "-ss <x>" where x is a timestamp.
 seekFF :: Time -> [Text]
-seekFF t = ["-ss", timeFF t]
+seekFF t = ["-ss", sformat timef t]
 
 -- | "-ss <x> -to <y> <output>".
 rangeFF :: Range -> Path b File -> [Text]
-rangeFF (Range f t) x = seekFF f ++ ["-to", timeFF t, toFilePathText x]
+rangeFF (Range f t) x = seekFF f ++ ["-to", sformat timef t, sformat pathf x]
 
 -- | "-ss <x> -vframes 1 <output>"
 frameFF :: Time -> Path b File -> [Text]
-frameFF t x = seekFF t ++ ["-vframes", "1", toFilePathText x]
+frameFF t x = seekFF t ++ ["-vframes", "1", sformat pathf x]
 
 -- | "-i <output>"
 inputFF :: Path b File -> [Text]
-inputFF x = ["-i", toFilePathText x]
+inputFF x = ["-i", sformat pathf x]
 
 -- | "ffmpeg -y" followed by some arguments.
 runffmpeg :: MonadIO m => [Text] -> m ()
