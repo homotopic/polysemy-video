3f0827e88a63ff9dd6111cc0cff7fd42ff818015
Daniel Firth <dan.firth@homotopic.tech>
2021-07-18 17:54:28 +0200

    Rename interpreter functions and bump to v0.2.0.0

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,5 +1,11 @@
 # Changelog for polysemy-video
 
+## v0.2.0.0
+
+* Use `Time` from 
+  [simple-media-timestamp](https://hackage.haskell.org/package/simple-media-timestamp).
+* Rename interpreter functions.
+
 ## v0.1.1.0
 
 * Support both `Abs` and `Rel`.
--- src/Polysemy/Video.hs
+++ src/Polysemy/Video.hs
@@ -14,20 +14,31 @@
 --   Stability : experimental
 --
 -- Experimental Video processing DSL for Polysemy.
-module Polysemy.Video where
+module Polysemy.Video
+  ( ClipProcess (..),
+    extractAudio,
+    extractClips,
+    extractFrames,
+    runFFMpegCli,
+    traceFFMpegArgs,
+    ignoreClipProcess,
+  )
+where
 
 import Control.Monad.IO.Class
 import Data.Text (Text)
-import qualified Data.Text as T
 import Formatting
 import Media.Timestamp
 import Media.Timestamp.Formatting
 import Path
 import Path.Formatting
 import Polysemy
+import Polysemy.Trace
 import qualified Turtle as S
 
--- |
+-- | Effect for disecting a video file.
+--
+-- @since 0.1.1.0
 data ClipProcess m a where
   ExtractAudio :: Path b File -> [(Range, Path b' File)] -> ClipProcess m ()
   ExtractClips :: Path b File -> [(Range, Path b' File)] -> ClipProcess m ()
@@ -60,15 +71,34 @@ mktreeFP :: MonadIO m => Path b Dir -> m ()
 mktreeFP = S.mktree . S.decodeString . toFilePath
 
 -- | Interpret `ClipProcess` by running it against ffmpeg on the command line.
-interpretFFMpegCli :: Member (Embed IO) effs => Sem (ClipProcess ': effs) a -> Sem effs a
-interpretFFMpegCli = interpret $ \case
+--
+-- @since 0.2.0.0
+runFFMpegCli :: Member (Embed IO) effs => Sem (ClipProcess ': effs) a -> Sem effs a
+runFFMpegCli = interpret $ \case
   ExtractAudio x ts -> mapM_ mktreeFP (parent . snd <$> ts) >> runffmpeg (inputFF x <> (uncurry rangeFF =<< ts))
   ExtractClips x ts -> mapM_ mktreeFP (parent . snd <$> ts) >> runffmpeg (inputFF x <> (uncurry rangeFF =<< ts))
   ExtractFrames x ts -> mapM_ mktreeFP (parent . snd <$> ts) >> runffmpeg (inputFF x <> (uncurry frameFF =<< ts))
 
--- | Interpret `ClipProcess` by printing out the command it would have run to the terminal.
-interpretFFMpegNoop :: Member (Embed IO) effs => Sem (ClipProcess ': effs) a -> Sem effs a
-interpretFFMpegNoop = interpret $ \case
-  ExtractAudio x ts -> embed $ print $ T.unwords $ inputFF x <> (uncurry rangeFF =<< ts)
-  ExtractClips x ts -> embed $ print $ T.unwords $ inputFF x <> (uncurry rangeFF =<< ts)
-  ExtractFrames x ts -> embed $ print $ T.unwords $ inputFF x <> (uncurry frameFF =<< ts)
+-- | Trace `ClipProcess` by printing out the arguments it would pass to ffmpeg.
+--
+-- @since 0.2.0.0
+traceFFMpegArgs :: Members '[ClipProcess, Trace] r => Sem r a -> Sem r a
+traceFFMpegArgs = intercept $ \case
+  ExtractAudio x ts -> do
+    trace $ show $ inputFF x <> (uncurry rangeFF =<< ts)
+    extractAudio x ts
+  ExtractClips x ts -> do
+    trace $ show $ inputFF x <> (uncurry rangeFF =<< ts)
+    extractClips x ts
+  ExtractFrames x ts -> do
+    trace $ show $ inputFF x <> (uncurry frameFF =<< ts)
+    extractFrames x ts
+
+-- | Noop the `ClipProcess` effect.
+--
+-- @since 0.2.0.0
+ignoreClipProcess :: Sem (ClipProcess ': r) a -> Sem r a
+ignoreClipProcess = interpret $ \case
+  ExtractAudio _ _ -> pure ()
+  ExtractClips _ _ -> pure ()
+  ExtractFrames _ _ -> pure ()
